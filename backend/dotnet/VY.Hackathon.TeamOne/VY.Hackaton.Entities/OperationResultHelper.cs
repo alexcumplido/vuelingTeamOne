4a3cd41e3f30200256f24aca31e051b7efd17396
lanuzalorenzo <39988858+lanuzalorenzo@users.noreply.github.com>
2023-02-21 13:26:58 +0100

    [Back] Add new layer of infra

---
+++ backend/dotnet/VY.Hackathon.TeamOne/VY.Hackaton.Entities/OperationResultHelper.cs
@@ -0,0 +1,12 @@
+﻿using VY.Hackaton.TeamOne.ProviderData.Infrastructure.Contracts.Models;
+
+namespace VY.Hackaton.Entities
+{
+    public static class OperationResultHelper
+    {
+        public static OperationResult<T> GenerateOperationResult<T>(T result) where T : class => new(result, Array.Empty<Error>());
+
+        public static OperationResult<T> GenerateOperationResultWithError<T>(Exception ex) where T : class =>
+            new(null!, new[] {new Error(ex.Message)});
+    }
+}
