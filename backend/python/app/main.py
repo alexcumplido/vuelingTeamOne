7d130e50b72884447f84a95ca231c465d021f1df
lanuzalorenzo <39988858+lanuzalorenzo@users.noreply.github.com>
2023-02-21 15:04:23 +0100

    [phyton] change methods in fastapi

--- backend/python/app/main.py
+++ backend/python/app/main.py
@@ -1,17 +1,11 @@
-from typing import Union
+from typing import List
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/simple/{handling_area}/{date}")
-def read_simple(handling_area: str, date: str):
-    return {  }
+@app.get("/getData/{parameters}")
+def read_data(parameters: List[str]):
+    return { 
 
-@app.get("/allAreas/{date}")
-def read_all_areas(date: str):
-    return {}
-
-@app.get("/week/{start_date}/{end_date}")
-def read_all_areas_per_week(start_date: str, end_date: str):
-    return {}
+     }
